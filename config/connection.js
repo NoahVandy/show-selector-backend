2f519163cd1966b4cad366a87ef731ed89d937b8
NoahVandy <nvandervel@my.gcu.edu>
2022-06-21 15:00:43 -0500

    changing to cloud atlas

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,14 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
 
 mongoose.connect(
   process.env.MONGODB_URI ||
-    "mongodb+srv://Skronkie:Colby6213@cluster0.6eobr.mongodb.net/?retryWrites=true&w=majority",
+    "mongodb+srv://show-selector-admin:NoahAndEthan<3@cluster0.eriyx.mongodb.net/?retryWrites=true&w=majority",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
     useFindAndModify: false,
   }
-);
+)
 
-module.exports = mongoose.connection;
+module.exports = mongoose.connection
